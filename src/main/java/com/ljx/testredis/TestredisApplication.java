a9d55de65602e9bdef30d0e1e23c20b49feea9b0
jasonliu_home <ljx407@163.com>
2019-09-14 15:06:09 +0800

    modify to tomcat start

--- src/main/java/com/ljx/testredis/TestredisApplication.java
+++ src/main/java/com/ljx/testredis/TestredisApplication.java
@@ -2,12 +2,18 @@ package com.ljx.testredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestredisApplication {
+public class TestredisApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TestredisApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestredisApplication.class);
+    }
 }
--- src/main/java/com/ljx/testredis/controller/HelloController.java
+++ src/main/java/com/ljx/testredis/controller/HelloController.java
@@ -1,17 +1,20 @@
 package com.ljx.testredis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("hello")
+@RequestMapping
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam(name = "name") String name , @RequestParam("msg") String message) {
+        name = StringUtils.isEmpty(name) ? "ljx" : name;
+        message = StringUtils.isEmpty(message) ? "hello world" : message;
         return String.format("hello %s , %s",name,message);
     }
 }
